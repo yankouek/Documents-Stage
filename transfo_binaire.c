8a3e652f441ab31864c8cad329d2990a39f506a0
Tehe Bommanin Yannick <yankouek@yahoo.fr>
2017-08-28 11:27:33 +0200

    Add files via upload

---
+++ transfo_binaire.c
@@ -0,0 +1,15 @@
+//essai du code c sous gcc et linux
+#include <stdio.h>
+#include<stdlib.h>
+ 
+int main()
+{int i ;
+	char buffer[100];
+	while(fgets(buffer,100,stdin)){
+		i=atoi(buffer);
+		//fwrite(&i,sizeof(int),1,stdout);
+		fwrite(&i,sizeof(int),1,stdout);
+	}
+	
+ return 0;
+ }
